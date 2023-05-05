95a972c0e2f3b5f3d3d0b5fce4af3a07339f805c
Eric Sampaio <ericsmp18@gmail.com>
2023-05-05 11:05:37 -0300

    Alteracação no form e modificação na função

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function verificar(){
   const nome = document.getElementById('codigo').value;
   const senha = document.getElementById('senha').value
-  if(nome === 'Eric', 'Gabriel', 'Gabriele', 'Daniel' && senha === 'devbot123'){
+  if(nome === 'Eric', 'Gabriel', 'Gabriele', 'Daniel' && senha === "devbot123" ){
     console.log(true)
     document.getElementById('resultado').innerHTML= `Seja bem-vindo ${nome}`
   }else{
     console.log(false)
-
+    document.getElementById('resultado').innerHTML= `Não foi encontrado você nos registro`
   }
 
 
