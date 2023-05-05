a9590d339e8ce7692d1448b553ea4faad6a66f3b
Eric Sampaio <ericsmp18@gmail.com>
2023-05-05 11:18:47 -0300

    modificação no javaScript deixando a vizulização do true e false no site

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-function verificar(){
+function verificar(event){
   const nome = document.getElementById('codigo').value;
   const senha = document.getElementById('senha').value
-  if(nome === 'Eric', 'Gabriel', 'Gabriele', 'Daniel' && senha === "devbot123" ){
+  if(nome === 'Eric', 'Gabriel', 'Gabriele', 'Daniel' && senha === "devbot123"  ){event.preventDefault() 
     console.log(true)
     document.getElementById('resultado').innerHTML= `Seja bem-vindo ${nome}`
   }else{
@@ -11,3 +11,4 @@ function verificar(){
 
 
 }
+
