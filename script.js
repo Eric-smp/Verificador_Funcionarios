6a321ae1104b53f74c2ece65f670d7d933672bb5
Eric Sampaio <ericsmp18@gmail.com>
2023-05-05 14:52:31 -0300

    Alteração na função

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 function verificar(event){
   const nome = document.getElementById('codigo').value;
   const senha = document.getElementById('senha').value
-  if(nome === 'Eric', 'Gabriel', 'Gabriele', 'Daniel' && senha === "devbot123"  ){event.preventDefault() 
+  if(nome === 'Eric', 'Gabriel', 'Gabriela', 'Daniel' && senha === "devbot123"  ){
+    event.preventDefault() 
     console.log(true)
     document.getElementById('resultado').innerHTML= `Seja bem-vindo ${nome}`
   }else{
+    event.preventDefault() 
     console.log(false)
     document.getElementById('resultado').innerHTML= `Não foi encontrado você nos registro`
   }
