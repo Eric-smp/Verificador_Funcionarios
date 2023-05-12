b0795db2b080c613aad45f41eba4ea9efb9f9c12
Eric Sampaio <eric.sampaio@devbot.com.br>
2023-05-12 14:28:13 -0300

    Alteração no js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function verificar(event){
   const nome = document.getElementById('codigo').value;
   const senha = document.getElementById('senha').value
-  if(nome === 'Eric', 'Gabriel', 'Gabriela', 'Daniel' && senha === "devbot123"  ){
+  if(nome === 'Eric'|| nome === 'Gabriela' && senha === "devbot123"  ){
     event.preventDefault() 
     console.log(true)
     document.getElementById('resultado').innerHTML= `Seja bem-vindo ${nome}`
@@ -14,3 +14,19 @@ function verificar(event){
 
 }
 
+
+
+class Pessoa{
+  constructor(nome, id){
+    this.nome = nome;
+    this.id = id 
+
+
+  }
+
+}
+
+
+const eric = new Pessoa('Eric', 5156)
+
+console.log(eric)
